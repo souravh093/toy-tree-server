8134b2807b44205b828e122626a1dc877913cf11
Sourav halder <souravh093@gmail.com>
2023-05-18 17:34:09 +0600

    get data from client side and post this data in mongodb

--- index.js
+++ index.js
@@ -9,12 +9,11 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Toy Tree Server is running')
-})
+app.get("/", (req, res) => {
+  res.send("Toy Tree Server is running");
+});
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ukmkwhb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,16 +22,33 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const toyCollection = client.db("toysDB").collection("toys");
+
+    app.get('/toys', async(req, res) => {
+        const result = await toyCollection.find().toArray();
+        res.send(result)
+    })
+
+    app.post("/addToys", async(req, res) => {
+        const doc = req.body;
+        const result = await toyCollection.insertOne(doc)
+        res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -40,7 +56,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-    console.log(`server is running on port ${port}`)
-})
+  console.log(`server is running on port ${port}`);
+});
